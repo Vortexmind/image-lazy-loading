7f2b252f4c0adfa402830da6672906f8c6d0d5f6
Vortexmind <Vortexmind@users.noreply.github.com>
2020-07-02 10:31:33 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,42 @@
+addEventListener('fetch', event => {
+  event.respondWith(handleRequest(event))
+})
+
+const EXCLUDED_PATHS = ["/ghost","/rss","/content","/assets"]
+const IMG_SELECTORS = ["kg-image", "post-card-image", "feature-image"]
+const LOCAL_CONTENT_URL = BLOG_URL+"content/images/"
+const CDN_FETCH_URL = CLOUDINARY_URL+"image/fetch/q_auto,f_auto,dpr_auto/"
+
+class ElementHandler {
+  element(element) {
+
+      const imgClass = element.getAttribute('class') || ''
+      const imgSrc = element.getAttribute('src') || ''
+
+      if (IMG_SELECTORS.some( val => imgClass.includes(val) )) {
+
+        // Add lazy loading if not defined already
+        if(!element.getAttribute('loading')  ) {
+          element.setAttribute('loading', 'lazy')
+        }
+
+        // If image is loaded from local content, rewrite to pull from CDN
+        if(imgSrc.startsWith(LOCAL_CONTENT_URL)) {
+          element.setAttribute('src',CDN_FETCH_URL+imgSrc)
+        }
+
+    }
+  }
+}
+
+async function handleRequest(event) {
+  
+  const pathname = new URL(event.request.url).pathname.toLowerCase()
+  const res = await fetch(event.request)
+  
+  if (EXCLUDED_PATHS.some( val => pathname.startsWith(val) )) { 
+    return res
+  } else { 
+    return new HTMLRewriter().on('img', new ElementHandler()).transform(res)
+  }
+}
